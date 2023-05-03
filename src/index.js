e8a54b527ffed3f0b4ddaf79c8108035d663df31
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-05-03 13:25:26 +0430

    adding html css and jd file into src folder

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const tasks = [
+    {
+        description: "Buy groceries",
+        completed: false,
+        index:0
+    },
+    {
+        description: "clean the house",
+        completed: false,
+        index:1
+    },
+    {
+        description: "working on To Do list",
+        completed: false,
+        index:2
+    }
+];
+
+const listContainer = document.getElementById('list-container');
+const ShowList = () => {
+  listContainer.innerHTML = '';
+  for (let i = 0; i < tasks.length; i += 1) {
+    const listItem = document.createElement('li');
+    const checkIcon = document.createElement('input');
+    const span = document.createElement('span');
+    const dotsIcon = document.createElement('img');
+
+    listItem.id = `task${tasks[i].index}`;
+    checkIcon.type = 'checkbox';
+    checkIcon.checked = tasks[i].completed;
+    span.innerHTML = tasks[i].description;
+    dotsIcon.src = "dots.png"
+    dotsIcon.style.width = "20px";
+    listItem.appendChild(checkIcon);
+    listItem.appendChild(span);
+    listItem.appendChild(dotsIcon);
+    listContainer.appendChild(listItem);
+  }
+};
+ShowList();
